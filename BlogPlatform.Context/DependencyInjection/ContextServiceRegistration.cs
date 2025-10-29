516282d0256d44f7d10bf2d2a122c017b51d67e7
Abdullah <anasbutt2014@gmail.com>
2025-10-29 18:07:45 +0400

    Rebuilded some nuget packages

--- BlogPlatform.Context/DependencyInjection/ContextServiceRegistration.cs
+++ BlogPlatform.Context/DependencyInjection/ContextServiceRegistration.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,7 +11,19 @@ namespace BlogPlatform.Context.DependencyInjection
     {
         public static IServiceCollection AddContextRegistration(this IServiceCollection services, IConfiguration config)
         {
-            services.AddDbContext<BlogDbContext>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<BlogDbContext>(options =>
+            {
+                var connection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+
+                // Acquire access token using Managed Identity
+                var credential = new DefaultAzureCredential();
+                var token = credential.GetToken(
+                    new TokenRequestContext(new[] { "https://database.windows.net/.default" }));
+
+                connection.AccessToken = token.Token;
+
+                options.UseSqlServer(connection);
+            });
 
             return services;
         }
